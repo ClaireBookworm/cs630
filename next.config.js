2bb20bdcc5fea03bb68765908013a155966aa75f
Claire Wang <clara32356@gmail.com>
2022-09-18 18:46:47 -0400

    initial

--- next.config.js
+++ next.config.js
@@ -5,3 +5,11 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// this is for NEXTRA (experiment)
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+module.exports = withNextra()
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'nextra-theme-docs/style.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
